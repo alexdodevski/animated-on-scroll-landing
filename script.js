d78a3e5e54e29963062af5819b415f7faeac819f
Alexander Dodevski <36091237+alexdodevski@users.noreply.github.com>
2022-02-03 00:39:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+class AnimatedElement {
+    constructor(elem) {
+        this.elem = elem
+        this.elemClass = elem.classList[0]
+    }
+}
+
+class App {
+    constructor(elems) {
+        this.items = elems
+    }
+
+    animatedOnScroll() {
+        if (this.items.length == 0) return
+        this.animatedItems()
+        const animatedItems = this.animatedItems.bind(this)
+        document.addEventListener('scroll', animatedItems)
+    }
+
+    animatedItems() {
+
+
+    }
+}
+
+function getItems() {
+    const items = document.querySelectorAll('._anim_items')
+    let elemsArr = []
+
+    for (let i = 0; i < items.length; i++) {
+        let item = new AnimatedElement(items[i])
+        elemsArr.push(item)
+    }
+
+    const app = new App(elemsArr)
+    app.animatedOnScroll()
+    console.log(app)
+}
+
+getItems()
